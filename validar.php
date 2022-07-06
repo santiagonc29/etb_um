97efe76142d44bc98800a3bf4cd618cc45fb839a
Santiago <snarinocardenas@gmail.com>
2022-07-06 11:36:24 -0500

    Validación de cargo por if

--- validar.php
+++ validar.php
@@ -6,42 +6,30 @@ $password=$_POST['password'];
 echo $usuario;
 echo $password;
 
-$cons="SELECT*FROM tbl_um_supervisores where login='$usuario' and clave='$password'";
+//se define la consulta en una varible
+$cons="SELECT cargo FROM tbl_um_supervisores where login='$usuario' and clave='$password'";
+    
+    //Almacena la consulta
     $stid = oci_parse($conexión, $cons);
-        if (!$stid) {
-            $e = oci_error($conexión);
-            trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-        }// Realizar la lógica de la consulta
+    //ejecuta la consulta    
+    $r = oci_execute($stid);
         
-        $r = oci_execute($stid);
-        if (!$r) {
-            $e = oci_error($stid);
-            trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
-        }// Obtener los resultados de la consulta
-        
-        print "<table border='1'>\n";
-        while ($fila = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
-            print "<tr>\n";
-            foreach ($fila as $elemento) {
-                print "    <td>" . ($elemento !== null ? htmlentities($elemento, ENT_QUOTES) : "") . "</td>\n";
-            }
-           
-        }
-        
+
        $fila = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS);
 
+       $str = implode(" ", $fila);
 
+       print $str;
 
-if($fila){
-  
-    header('location: home.php');
+       if($str == "ADMINISTRADOR"){
 
-}else{
-    ?>
-   
-  <h1 class="bad">ERROR DE AUTENTIFICACION</h1>
- <?php
-}
+        header('location: home.php');
+
+       }else if($str == "SUPERVISOR"){
+        
+        header('location: sup.php');
+        
+       }
 
 oci_free_statement($stid);
  oci_close($conexión);
